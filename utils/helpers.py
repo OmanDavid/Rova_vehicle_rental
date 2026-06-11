721011a7c8c63145fbb1d0de5f450e2d139b7ab2
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-06-11 15:44:00 +0300

    Add password validation function to helpers

--- utils/helpers.py
+++ utils/helpers.py
@@ -4,6 +4,9 @@ def validate_username(username):
     if not username.strip():
         return False, "Incorrect username, try again!"
     return True, ""
-
-
-
+    
+def validate_password(password_hashed):
+    if len(password_hashed) < 5:
+        return False, "Password must be more than 5 characters long"
+    return True, ""
+    
