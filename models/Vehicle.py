3d385c115b72abcc1b9f23c952da9592be3771d0
Ccode-400 <stanleymngige@gmail.com>
2026-06-11 15:09:34 +0300

    added init constructor

--- models/Vehicle.py
+++ models/Vehicle.py
@@ -4,3 +4,15 @@ class Vehicle:
     """
     Represents a vehicle that can be listed for booking.
     """
+
+    def __init__(
+        self,
+        owner,
+        category,
+        brand,
+        model,
+        year,
+        price_per_day,
+        available=True,
+        vehicle_id=None,
+    ):
