7b1c9fd8e3224e9f9ebc89aac55b1b234c13b35c
Ccode-400 <stanleymngige@gmail.com>
2026-06-11 17:07:45 +0300

    added directory paths

--- lib/sample_data.py
+++ lib/sample_data.py
@@ -6,3 +6,13 @@ from models.Vehicle import Vehicle
 from models.Customer import Customer
 from models.Booking import Booking
 from utils.auth import hash_password
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+DATA_DIR = os.path.join(BASE_DIR, "data")
+
+USERS_FILE = os.path.join(DATA_DIR, "users.json")
+VEHICLES_FILE = os.path.join(DATA_DIR, "vehicle.json")
+CUSTOMERS_FILE = os.path.join(DATA_DIR, "customers.json")
+BOOKINGS_FILE = os.path.join(DATA_DIR, "bookings.json")
+
+os.makedirs(DATA_DIR, exist_ok=True)
